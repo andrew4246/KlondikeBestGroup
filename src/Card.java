11def9e509a19974462656614222cd552ac211ad
Student <Student@avd8pc005.psn.pleasanton.k12.ca.us>
2022-02-09 11:35:56 -0800

    updated card to include unfinished update and draw

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,19 @@
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
