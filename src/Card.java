a076af9d744c6ebc2b6e931fecf66d5b0188832b
andrew4246 <99358218+andrew4246@users.noreply.github.com>
2022-02-16 10:29:23 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -4,6 +4,10 @@ import java.awt.event.ActionEvent;
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
 
+    private int suit;
+    private int value;
+    private boolean face;
+    
     @Override
     public void update(ActionEvent a) {
         // TODO Auto-generated method stub
