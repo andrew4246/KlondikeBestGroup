6343533f34666bc4fafc434299840f744954fa4a
Student <Student@avd8pc005.psn.pleasanton.k12.ca.us>
2022-02-16 11:34:43 -0800

    completed the constructor for Card.java import java.awt.event.ActionEvent;

--- src/Card.java
+++ src/Card.java
@@ -1,16 +1,37 @@
 import java.awt.Graphics;
-import java.awt.event.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.imageio.ImageIO;
 
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
 
-    private int suit, value, x, y;
+    private int suit, value, color, x, y;
     private boolean faceUp;
+    private String[] suitRef = {"c", "s", "h", "d"};
+    Image frontImage, backImage;
 
+/** Constructor for a new Card. Takes in the suit, value, x coordinate, and y coordinate. The suit is an int (0-3) 0 = club, 1 = spade, 2 = heart, 3 = daimond. The color of the card (0 - black, 1 - red) is determined from the suit.
+*/
     public Card(int suit, int value, int x, int y){
         this.suit = suit;
         this.value = value;
         faceUp = false;
+
+        String fileName = "";
+        fileName += "images/cards/";
+        fileName += suitRef[suit];
+        fileName += value;
+        fileName += ".png";
+
+        try {
+			frontImage = ImageIO.read(new File(fileName));
+			backImage = ImageIO.read(new File("images/cards/b1fv.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     public void draw(Graphics g){
