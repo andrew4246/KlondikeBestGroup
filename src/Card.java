392b003f1152567c70d5b8af342a71fe13e671af
andrew4246 <99358218+andrew4246@users.noreply.github.com>
2022-02-16 10:40:16 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,23 +1,26 @@
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
 
     private int suit;
     private int value;
-    private boolean face;
-    
-    @Override
-    public void update(ActionEvent a) {
-        // TODO Auto-generated method stub
-        
+    private boolean faceUp; 
+    private Location L;
+
+    public Card(int suit, int value, Location L){
+        this.suit = suit;
+        this.value = value;
+        faceUp = false;
+        this.L = L;
     }
 
-    @Override
-    public void draw(Graphics g) {
-        // TODO Auto-generated method stub
-        
+    public void draw(Graphics g){
+
     }
 
+    public void update(ActionEvent e){
+
+    }
 }
