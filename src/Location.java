5c43f044a80cdf8290d09872e6446b117f90a66d
andrew4246 <99358218+andrew4246@users.noreply.github.com>
2022-02-16 10:37:22 -0800

    Create Location.java

---
+++ src/Location.java
@@ -0,0 +1,17 @@
+public class Location {
+    private int y;
+    private int x;
+
+    public Location(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+}
