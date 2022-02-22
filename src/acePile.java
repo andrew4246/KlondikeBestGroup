2242df053183e76aa2cf76d68a4e7bc8dbcf1928
963358 <73217236+963358@users.noreply.github.com>
2022-02-22 11:38:53 -0800

    Update acePile.java

--- src/acePile.java
+++ src/acePile.java
@@ -2,9 +2,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class acePile extends Pile{
-
+    private ArrayList<Card> list = new ArrayList<Card>();
     public acePile(int x, int y){
         super(x, y);
     }
@@ -20,11 +21,18 @@ public class acePile extends Pile{
 			e.printStackTrace();
 		}
     }
+    private void addCard(Card c){
+        list.add(c);
+    }
+    private void removeCards(int length){ // make a return method to see what cards were removed
+        for(int i = 0; i < length; i++){
+            list.remove(list.size()-1);
+        }
+    }
 
     @Override
     public void update(ActionEvent a) {
         // TODO Auto-generated method stub
-        
     }
 
     @Override
