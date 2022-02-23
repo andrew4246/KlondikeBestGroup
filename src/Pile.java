105f236302503740951accb033f5f4e62df42ef9
963358 <73217236+963358@users.noreply.github.com>
2022-02-23 10:36:44 -0800

    Update Pile.java

--- src/Pile.java
+++ src/Pile.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.AnnotatedWildcardType;
-import java.util.ArrayList;
 
 /** A Pile is a collection of cards.  This needs to be
  * Drawable because it will be shown on the GUI. Put code
@@ -8,15 +6,29 @@ import java.util.ArrayList;
  * be implemented here.  Updateable may have empty implementation.
  * You WILL write subclasses of Pile
  */
+import java.util.*;
 public abstract class Pile implements Drawable, Updateable {
-    private int x, y;
-    private ArrayList <Card> pile = new ArrayList<>();
-
-    public Pile (int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-    
     
     public abstract boolean canAddCard(Card c);
+    protected int x;
+    protected int y;
+    protected ArrayList<Card> list = new ArrayList<Card>();
+    
+    public Pile(int xVal, int yVal, ArrayList<Card> lst){
+        this.x = xVal;
+        this.y = yVal;
+        this.list = lst;
+    }
+
+    private void addCard(Card c){
+        list.add(c);
+    }
+
+    private void removeCards(int length){ // make a return method to see what cards were removed
+        for(int i = 0; i < length; i++){
+            list.remove(list.size()-1);
+        }
+    }
+
+
 }
