0246007812f96a3e5336eef59e29a90e0c714f4a
963358 <73217236+963358@users.noreply.github.com>
2022-02-25 11:10:18 -0800

    Create normalPile.java

---
+++ src/normalPile.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+import java.util.ArrayList;
+
+public class acePile extends Pile{
+
+    public acePile(int x, int y, ArrayList<Card> cards){
+        super(x, y, cards);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+		g.setColor(new Color(40, 155, 70));
+        g.drawRect(x, y, GameBoard.xDim, GameBoard.yDim);
+
+    }
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean canAddCard(Card c) {
+        if(this.list.size() == c.getValue() ){
+            return true;
+        }
+        return false;
+    }
+
+}
