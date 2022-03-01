5edacc80ac7edd0cefae1a7fe1db4ca2b356d90e
andrew4246 <99358218+andrew4246@users.noreply.github.com>
2022-03-01 11:23:42 -0800

    Add files via upload

---
+++ src/SidePile.java
@@ -0,0 +1,30 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+public class SidePile extends Pile{
+
+    public SidePile(int x, int y, ArrayList<Card> lst) {
+        super(x, y, lst);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean canAddCard(Card c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
